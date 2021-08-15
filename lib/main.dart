6fcfd152fd6c23f5a3b1ded1b43ef97989061e14
Joao Vitor R <joaovitor_new2@live.com>
2021-08-15 12:47:27 -0300

    add more providers and pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:beecontrol/core/app_theme.dart';
+import 'package:beecontrol/models/feed.dart';
 import 'package:beecontrol/models/weather.dart';
-// import 'package:beecontrol/pages/news/news_page.dart';
-import 'package:beecontrol/pages/register/register_page.dart';
+import 'package:beecontrol/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -22,8 +22,11 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
         providers: [
           ChangeNotifierProvider<Weather>(
-              create: (_) =>
-                  Weather(iconCode: '01d', id: 121998, temperature: 0, time: 0))
+              create: (_) => Weather(
+                  iconCode: '01d', id: 121998, temperature: 0, time: 0)),
+          ChangeNotifierProvider<Feed>(
+            create: (_) => Feed(),
+          )
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
@@ -35,7 +38,7 @@ class MyApp extends StatelessWidget {
           supportedLocales: const [Locale('pt', 'BR')],
           title: 'Bee Control',
           theme: AppTheme.lightTheme,
-          home: RegisterPage(),
+          home: HomePage(),
         ));
   }
 }
