4cdf38f9038e2bc4acbc91f22b4a67bf677c8047
Joao Vitor R <joaovitor_new2@live.com>
2021-08-24 16:54:47 -0300

    remove rss app

--- lib/services/feed/feed_repository.dart
+++ lib/services/feed/feed_repository.dart
@@ -5,9 +5,9 @@ import 'package:webfeed/webfeed.dart';
 class FeedRepository {
   Future getFeed() async {
     final client = http.Client();
-    Uri uri = Uri.parse('https://rss.app/feeds/QbeTGBmSCRd9yKVS.xml');
-    //'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSkwyMHZNREYyYmpNMUVnVndkQzFDVWlnQVAB?hl=pt-BR&gl=BR&ceid=BR:pt-419');
-    
+    Uri uri = Uri.parse(//'https://rss.app/feeds/QbeTGBmSCRd9yKVS.xml');
+        'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSkwyMHZNREYyYmpNMUVnVndkQzFDVWlnQVAB?hl=pt-BR&gl=BR&ceid=BR:pt-419');
+
     final response = await client.get(uri);
     if (response.statusCode != 200) {
       throw HTTPException(response.statusCode, "unable to fetch feed data");
