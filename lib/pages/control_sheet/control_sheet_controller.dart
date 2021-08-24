2d2f4fca373dbb2ae07882441a905e772ccc4547
Joao Vitor R <joaovitor_new2@live.com>
2021-08-24 16:56:57 -0300

    fix Report CRUD

---
+++ lib/pages/control_sheet/control_sheet_controller.dart
@@ -0,0 +1,35 @@
+import 'package:beecontrol/models/apiary.dart';
+import 'package:beecontrol/models/bee_hive.dart';
+import 'package:beecontrol/models/report.dart';
+import 'package:beecontrol/utils/constants.dart';
+import 'package:hive/hive.dart';
+
+class ControlSheetController {
+  Report report = Report(resume: []);
+  Apiary apiary = Apiary(hives: [], reports: []);
+
+  void addHive() {
+    apiary.hives.add(BeeHive(
+        name: 'Colméia ${apiary.hives.length + 1}',
+        situation: [],
+        production: []));
+    report.numHives = apiary.hives.length;
+  }
+
+  void saveReport() {
+    report.numHives = apiary.hives.length;
+    apiary.lastVisit = report.date;
+    int index = int.parse(report.name.substring(18));
+
+    if (apiary.reports.length >= index) {
+      apiary.reports[apiary.reports
+          .indexWhere((element) => element.date == report.date)] = report;
+    } else {
+      apiary.reports.add(report);
+    }
+
+    apiary.visits = apiary.reports.length;
+
+    Hive.box<Apiary>(CONSTANTS.box).put(apiary.id, apiary);
+  }
+}
--- lib/pages/control_sheet/control_sheet_page.dart
+++ lib/pages/control_sheet/control_sheet_page.dart
@@ -1,4 +1,6 @@
+import 'package:beecontrol/models/apiary.dart';
 import 'package:beecontrol/models/report.dart';
+import 'package:beecontrol/pages/control_sheet/control_sheet_controller.dart';
 import 'package:beecontrol/pages/control_sheet/widgets/bee_pasture.dart';
 import 'package:beecontrol/pages/control_sheet/widgets/comments.dart';
 import 'package:beecontrol/pages/control_sheet/widgets/goals_card.dart';
@@ -7,20 +9,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:ionicons/ionicons.dart';
-import 'package:provider/provider.dart';
 import 'package:beecontrol/core/app_text_style.dart';
 import 'package:beecontrol/core/app_theme.dart';
-import 'package:beecontrol/models/apiary.dart';
 import 'package:beecontrol/shared/circular_button.dart';
 import 'package:beecontrol/shared/empty_widget.dart';
 import 'package:beecontrol/shared/info_card.dart';
+import 'package:provider/provider.dart';
 
 class ControlSheetPage extends StatefulWidget {
   const ControlSheetPage({
     Key? key,
-    // required this.apiary,
+    required this.report,
   }) : super(key: key);
 
+  final Report report;
   // final Apiary apiary;
 
   @override
@@ -29,18 +31,19 @@ class ControlSheetPage extends StatefulWidget {
 
 class _ControlSheetPageState extends State<ControlSheetPage> {
   final formKey = GlobalKey<FormState>();
-
-  Report report = Report(resume: []);
-  Apiary apiary = Apiary(hives: [], reports: []);
-
   TextEditingController textController = TextEditingController();
+  ControlSheetController controller = ControlSheetController();
 
   @override
   void initState() {
-    apiary = context.read<Apiary>();
-    report = context.read<Report>();
-    textController.text = report.comments;
+    controller.report = Report.fromJson(widget.report.toJson());
+    textController.text = controller.report.comments;
+    controller.apiary = Apiary.fromJson(context.read<Apiary>().toJson());
     super.initState();
+  }
+
+  void onChanged(String obs) {
+    controller.report.comments = obs;
   }
 
   @override
@@ -57,7 +60,7 @@ class _ControlSheetPageState extends State<ControlSheetPage> {
                     CircularButton(
                         onTap: () => Navigator.of(context).pop(),
                         icon: Ionicons.chevron_back_outline),
-                    Text(report.name, style: AppTextStyle.boldTitle),
+                    Text(controller.report.name, style: AppTextStyle.boldTitle),
                     const SizedBox(width: 35, height: 35)
                   ],
                 )),
@@ -69,7 +72,9 @@ class _ControlSheetPageState extends State<ControlSheetPage> {
               FeatherIcons.plus,
               color: AppTheme.eclipse,
             ),
-            onPressed: () {},
+            onPressed: () => setState(() {
+              controller.addHive();
+            }),
           ),
           floatingActionButtonLocation:
               FloatingActionButtonLocation.centerFloat,
@@ -86,7 +91,9 @@ class _ControlSheetPageState extends State<ControlSheetPage> {
                             child: FadeInAnimation(child: widget),
                           ),
                       children: [
-                        GoalsApiaryCard(apiary: apiary, report: report),
+                        GoalsApiaryCard(
+                            apiary: controller.apiary,
+                            report: controller.report),
                         Padding(
                             padding: const EdgeInsets.symmetric(vertical: 10),
                             child: Container(
@@ -102,18 +109,22 @@ class _ControlSheetPageState extends State<ControlSheetPage> {
                                       padding: const EdgeInsets.symmetric(
                                           horizontal: 0),
                                       child: BeePasture(
-                                        groupValue: report.beePasture,
+                                        groupValue:
+                                            controller.report.beePasture,
                                         onChanged: (value) {
                                           setState(() {
-                                            report.beePasture = value;
+                                            controller.report.beePasture =
+                                                value;
                                           });
                                         },
                                       ))
                                 ],
                               ),
                             )),
-                        CommentsCard(textController: textController),
-                        if (apiary.hives.isEmpty)
+                        CommentsCard(
+                            textController: textController,
+                            onChanged: onChanged),
+                        if (controller.apiary.hives.isEmpty)
                           Column(
                             children: [
                               Padding(
@@ -133,15 +144,22 @@ class _ControlSheetPageState extends State<ControlSheetPage> {
                               shrinkWrap: true,
                               padding: EdgeInsets.symmetric(vertical: 5),
                               physics: NeverScrollableScrollPhysics(),
-                              itemCount: apiary.hives.length,
+                              itemCount: controller.apiary.hives.length,
                               itemBuilder: (context, index) {
-                                return HiveCard(hive: apiary.hives[index]);
+                                return HiveCard(
+                                    hive: controller.apiary.hives[index]);
                               }),
-                        if (apiary.hives.isNotEmpty)
+                        if (controller.apiary.hives.isNotEmpty)
                           Padding(
                             padding: const EdgeInsets.only(top: 20, bottom: 80),
                             child: ElevatedButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  controller.saveReport();
+                                  context
+                                      .read<Apiary>()
+                                      .updateProvider(controller.apiary);
+                                  Navigator.of(context).pop();
+                                },
                                 child: Text('Finalizar Relatório'),
                                 style: AppTheme.elevatedButtonStyle),
                           ),
