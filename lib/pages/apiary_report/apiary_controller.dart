a48c562e06c6c96916515539dfeeafc0f337e023
Joao Vitor R <joaovitor_new2@live.com>
2021-08-25 12:18:42 -0300

    add add Hive/Report

--- lib/pages/apiary_report/apiary_controller.dart
+++ lib/pages/apiary_report/apiary_controller.dart
@@ -4,7 +4,33 @@ import 'package:beecontrol/models/report.dart';
 class ApiaryController {
   Apiary apiary = Apiary(reports: []);
   Report report = Report(resume: [], hives: []);
+
   int orderBy = 0;
+
+  Report addReport() {
+    if (apiary.reports.isEmpty) {
+      return Report(
+          date: DateTime.now(),
+          name: 'Ficha de Controle ${apiary.reports.length + 1}',
+          resume: [],
+          newReport: true,
+          hives: []);
+    } else {
+      Report lastReport = apiary.reports.firstWhere((element) =>
+          element.name == 'Ficha de Controle ${apiary.reports.length}');
+      Report aux = Report(
+          date: DateTime.now(),
+          name: 'Ficha de Controle ${apiary.reports.length + 1}',
+          resume: [],
+          newReport: true,
+          hives: []);
+
+      aux.hives.addAll(lastReport.hives);
+
+      return aux;
+    }
+  }
+
   List<String> orderByList = [
     'Data (Decrescente)',
     'Data (Crescente)',
